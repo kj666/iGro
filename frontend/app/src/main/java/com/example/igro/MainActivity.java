383cd95baa08cb913799faced32963d5d2a7153b
Dimitri534 <dsevastis@gmail.com>
2019-02-28 13:06:38 -0500

    Latest update

--- frontend/app/src/main/java/com/example/igro/MainActivity.java
+++ frontend/app/src/main/java/com/example/igro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.igro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.R;
 
 
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.
 
         humidityButton = (Button) findViewById(R.id.)
     }
