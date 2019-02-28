d52baf8143158da4e8f0ba9a6e14743767c42998
mkanyar <33469024+mkanyar@users.noreply.github.com>
2019-02-27 23:13:36 -0500

    temperature degrees conversion
    degrees conversion on mainActivity and temperatureActivity

--- frontend/app/src/main/java/com/example/igro/MainActivity.java
+++ frontend/app/src/main/java/com/example/igro/MainActivity.java
@@ -10,16 +10,59 @@ import android.view.View;
 import android.widget.Button;
 
 
+
 public class MainActivity extends AppCompatActivity {
     private Button temperature;
     private Button number;
-
+    private Button celcius;
+    private Button fahrenheit;
+    private boolean celcius_pressed=true;
+    private boolean fahrenheit_pressed=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         temperature=(Button) findViewById(R.id.temp_button);
         number=(Button) findViewById(R.id.tempNumberView);
+        celcius=(Button) findViewById(R.id.celciusOutButton);
+        fahrenheit=(Button) findViewById(R.id.fahrenheitOutButton);
+
+
+        //from fahrenheit to celcius
+        celcius.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(fahrenheit_pressed) {
+                    for(int i=0;i<1;i++) {
+                        Integer degrees = Integer.parseInt(number.getText().toString());
+                         Integer a = (Integer)Math.round((degrees - 32) * 5 / 9);
+                        number.setText(Integer.toString(a));
+                    }
+                    celcius_pressed = true;
+                    fahrenheit_pressed=false;
+                }
+            }
+        });
+        // from celcius to fahrenheit
+        fahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(celcius_pressed) {
+                    for(int i=0;i<1;i++) {
+                        Integer degrees = Integer.parseInt(number.getText().toString());
+                        Integer a = degrees * 9 / 5 + 32;
+                        number.setText(Integer.toString(a));
+                    }
+                    fahrenheit_pressed = true;
+                    celcius_pressed=false;
+                }
+            }
+        });
+
+        //opening the Temperature view when the temperature text is clicked
         temperature.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,17 +70,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //opening the Temperature view when the temperature number is clicked
         number.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTemperature();
             }
         });
+
     }
     public void openTemperature(){
         Intent intent=new Intent(this,TemperatureActivity.class);
         startActivity(intent);
     }
+
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater=getMenuInflater();
--- frontend/app/src/main/java/com/example/igro/TemperatureActivity.java
+++ frontend/app/src/main/java/com/example/igro/TemperatureActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -13,15 +16,100 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class TemperatureActivity extends AppCompatActivity {
 
+    private boolean celcius_pressedGreen =true;
+    private boolean fahrenheit_pressedGreen =false;
+    private boolean celcius_pressedOut =true;
+    private boolean fahrenheit_pressedOut =false;
 
+    private Button celciusGreen;
+    private Button fahrenheitGreen;
+    private Button celciusOut;
+    private Button fahrenheitOut;
+    private TextView numberGreen;
+    private TextView numberOut;
     LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_temperature);
-        double y,x;
-        x=-5;
+        double y,x=-5;
+        numberGreen =(TextView) findViewById(R.id.indoorTempView);
+        numberOut=(TextView) findViewById(R.id.outTempView);
+        celciusGreen =(Button) findViewById(R.id.celciusGreenButton);
+        fahrenheitGreen =(Button) findViewById(R.id.fahrenheitGreenButton);
+        celciusOut =(Button) findViewById(R.id.celciusOutButton);
+        fahrenheitOut =(Button) findViewById(R.id.fahrenheitOutButton);
+
+
+        //from fahrenheitGreen to celciusGreen
+        celciusGreen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(fahrenheit_pressedGreen) {
+                    for(int i=0;i<1;i++) {
+                        Integer degrees = Integer.parseInt(numberGreen.getText().toString());
+                        Integer a = (Integer)Math.round((degrees - 32) * 5 / 9);
+                        numberGreen.setText(Integer.toString(a));
+                    }
+                    celcius_pressedGreen = true;
+                    fahrenheit_pressedGreen =false;
+                }
+            }
+        });
+        // from celcius to fahrenheit
+        fahrenheitGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(celcius_pressedGreen) {
+                    for(int i=0;i<1;i++) {
+                        Integer degrees = Integer.parseInt(numberGreen.getText().toString());
+                        Integer a = degrees * 9 / 5 + 32;
+                        numberGreen.setText(Integer.toString(a));
+                    }
+                    fahrenheit_pressedGreen = true;
+                    celcius_pressedGreen =false;
+                }
+            }
+        });
+
+        //from fahrenheit to celcius
+        celciusOut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(fahrenheit_pressedOut) {
+                    for(int i=0;i<1;i++) {
+                        Integer degrees = Integer.parseInt(numberOut.getText().toString());
+                        Integer a = (Integer)Math.round((degrees - 32) * 5 / 9);
+                        numberOut.setText(Integer.toString(a));
+                    }
+                    celcius_pressedOut = true;
+                    fahrenheit_pressedOut =false;
+                }
+            }
+        });
+        // from celciusGreen to fahrenheitGreen
+        fahrenheitOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(celcius_pressedOut) {
+                    for(int i=0;i<1;i++) {
+                        Integer degrees = Integer.parseInt(numberOut.getText().toString());
+                        Integer a = degrees * 9 / 5 + 32;
+                        numberOut.setText(Integer.toString(a));
+                    }
+                    fahrenheit_pressedOut = true;
+                    celcius_pressedOut =false;
+                }
+            }
+        });
+
 
         GraphView graph =findViewById(R.id.graphTempCardView);
         series=new LineGraphSeries<>();
