335904d5421af3e000c99774e900027c7ad0b609
Karthikan Jeyabalan <35607975+kj666@users.noreply.github.com>
2019-02-27 21:59:44 -0500

    Displaying data from firebase in main activity
    this is just a template

--- frontend/app/src/main/java/com/example/igro/MainActivity.java
+++ frontend/app/src/main/java/com/example/igro/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.igro;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,11 +9,48 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button temperature;
     private Button number;
+    private Button tempNumberButton;
+
+    public int tempD;
+    //Reference to collection in firestore
+    private CollectionReference tempRef = FirebaseFirestore.getInstance().collection("temperature");
+
+    //Get document from firestore
+    public void getTempData(String id){
+        tempRef.document(id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()){
+                        tempD = Integer.parseInt(document.getString("temp"));
+
+                        Log.d("WORKING", ""+tempD);
+                    }
+                    else{
+                        Log.d("ERROR", "No such document");
+                    }
+                }
+                number.setText(tempD+"");
+            }
+        });
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         temperature=(Button) findViewById(R.id.temp_button);
         number=(Button) findViewById(R.id.number_button);
+        getTempData("1");
         temperature.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
