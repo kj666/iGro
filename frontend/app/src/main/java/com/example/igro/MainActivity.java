2b7fd358dfc5af8cf4b18d9ebe0700eb2388564d
Karthikan Jeyabalan <35607975+kj666@users.noreply.github.com>
2019-03-01 19:59:19 -0500

    Revert "Mike"

--- frontend/app/src/main/java/com/example/igro/MainActivity.java
+++ frontend/app/src/main/java/com/example/igro/MainActivity.java
@@ -18,16 +18,9 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
-
 public class MainActivity extends AppCompatActivity {
     private Button temperature;
-    private Button uv;
-    private Button uvNumber;
     private Button number;
-    private Button celcius;
-    private Button fahrenheit;
-    private boolean celcius_pressed=true;
-    private boolean fahrenheit_pressed=false;
     private Button tempNumberButton;
 
     public int tempD;
@@ -64,76 +57,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         temperature=(Button) findViewById(R.id.temp_button);
-        number=(Button) findViewById(R.id.tempNumberView);
+        number=(Button) findViewById(R.id.number_button);
         getTempData("1");
-        number=(Button) findViewById(R.id.tempNumberView);
-        celcius=(Button) findViewById(R.id.celciusOutButton);
-        fahrenheit=(Button) findViewById(R.id.fahrenheitOutButton);
-        uv=(Button) findViewById(R.id.uvButton);
-        uvNumber=(Button)findViewById(R.id.uvNumberButton);
-
-
-        //from fahrenheit to celcius
-        celcius.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                if(fahrenheit_pressed) {
-                    for(int i=0;i<1;i++) {
-                        Double degrees = Double.parseDouble(number.getText().toString());
-                         Double a = (degrees - 32) * 5 / 9;
-                        number.setText(Double.toString(a));
-                    }
-                    celcius_pressed = true;
-                    fahrenheit_pressed=false;
-                }
-            }
-        });
-        // from celcius to fahrenheit
-        fahrenheit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(celcius_pressed) {
-                    for(int i=0;i<1;i++) {
-                        Double degrees = Double.parseDouble(number.getText().toString());
-                        Double a = degrees * 9 / 5 + 32;
-                        number.setText(Double.toString(a));
-                    }
-                    fahrenheit_pressed = true;
-                    celcius_pressed=false;
-                }
-            }
-        });
-
-        //opening the Temperature view when the temperature text is clicked
         temperature.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTemperature();
             }
         });
-        //opening the Uv index view  when the uv text is clicked
-        uv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUv();
-            }
-        });
 
-        //opening the Temperature view when the temperature number is clicked
         number.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTemperature();
-            }
-        });
-        ////opening the uv view when the uv number is clicked
-        uvNumber.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUv();
             }
         });
     }
@@ -141,13 +77,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this,TemperatureActivity.class);
         startActivity(intent);
     }
-    public void openUv(){
-        Intent intent=new Intent(this,UvIndexActivity.class);
-        startActivity(intent);
-    }
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater=getMenuInflater();
