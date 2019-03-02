6dfa68405b5cfc78ba5df49ed152a41c282ccdad
Versatire <47801574+Versatire@users.noreply.github.com>
2019-03-02 01:02:06 -0500

    heater control vlues to firebase
    Teperature activity, which contains heater control switch was modified so that the click of a switch sends a new value to firebase database heatSwitchEvents(id, dataTime, state) which records each time the switch was triggered on and off.
    Next: to read last value from the database to set the condition of the switch;
    next: create an extra activity to display the log of heater switch events and corresponding dates and times.

---
+++ frontend/app/src/main/java/com/example/igro/HeaterControlEvents.java
@@ -0,0 +1,48 @@
+package com.example.igro;
+
+public class HeaterControlEvents {
+
+    String heaterEventId;
+    String heaterEventDateTime;
+    Boolean heaterEventOnOff;
+
+    HeaterControlEvents(){
+
+    }
+
+    public HeaterControlEvents(String heaterEventId, String heaterEventDateTime, Boolean heaterEventOnOff) {
+        this.heaterEventId = heaterEventId;
+        this.heaterEventDateTime = heaterEventDateTime;
+        this.heaterEventOnOff = heaterEventOnOff;
+    }
+
+
+    public HeaterControlEvents(String heaterEventDateTime, Boolean heaterEventOnOff) {
+        this.heaterEventDateTime = heaterEventDateTime;
+        this.heaterEventOnOff = heaterEventOnOff;
+    }
+
+    public String getHeaterEventId() {
+        return heaterEventId;
+    }
+
+    public void setHeaterEventId(String heaterEventId) {
+        this.heaterEventId = heaterEventId;
+    }
+
+    public String getHeaterEventDateTime() {
+        return heaterEventDateTime;
+    }
+
+    public void setHeaterEventDateTime(String heaterEventDateTime) {
+        this.heaterEventDateTime = heaterEventDateTime;
+    }
+
+    public Boolean getHeaterEventOnOff() {
+        return heaterEventOnOff;
+    }
+
+    public void setHeaterEventOnOff(Boolean heaterEventOnOff) {
+        this.heaterEventOnOff = heaterEventOnOff;
+    }
+}
--- frontend/app/src/main/java/com/example/igro/TemperatureActivity.java
+++ frontend/app/src/main/java/com/example/igro/TemperatureActivity.java
@@ -2,24 +2,52 @@ package com.example.igro;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.DateFormat;
+
 
 public class TemperatureActivity extends AppCompatActivity {
 
-
     LineGraphSeries<DataPoint> series;
+
+    //initialize the layout fields
+    TextView tempControlTextView;
+    Switch tempSwitch;
+
+    //log tag to test the on/off state on changeState event of heaterSwitch
+    private static final String TAG = "HeaterIsOnTag";
+
+    //create heater database reference
+    DatabaseReference heaterSwitchEventDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_temperature);
+
+       tempControlTextView = (TextView)findViewById(R.id.tempControlTextView);
+       tempSwitch = (Switch)findViewById(R.id.tempSwitch);
+        Boolean switchState = tempSwitch.isChecked();
+
+        //
+        heaterSwitchEventDB = FirebaseDatabase.getInstance().getReference("heaterControl");
+
+
+
         double y,x;
         x=-5;
 
@@ -32,6 +60,66 @@ public class TemperatureActivity extends AppCompatActivity {
         }
          graph.addSeries(series);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        tempControlTextView = (TextView)findViewById(R.id.tempControlTextView);
+        tempSwitch = (Switch)findViewById(R.id.tempSwitch);
+        Boolean switchState = tempSwitch.isChecked();
+
+        tempSwitch.setClickable(true);
+
+        if(switchState == true){
+            Log.d(TAG, "The heater is on");
+        }else{
+            Log.d(TAG, "The heater is off");
+        }
+
+        tempSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton tempSwitch, boolean tempSwitchState){
+
+                //Call heaterSwitchEvent function
+                heaterSwitchEvent(tempSwitchState);
+
+
+            }
+        });
+
+
+    }
+
+
+    private void heaterSwitchEvent(boolean tempSwitchState) {
+
+        //record the time of the click
+        DateFormat heatOnDateTime = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+
+        //generate unique key for each switch, create a new object of HeaterControlEvents, record on/off & date/time in firebase
+        String heatEventId = heaterSwitchEventDB.push().getKey();
+
+        if(!(heatEventId==null)) {
+            HeaterControlEvents heatSwitchClickEvent = new HeaterControlEvents(heatEventId, heatOnDateTime.toString(), tempSwitchState);
+            heaterSwitchEventDB.child(heatEventId).setValue(heatSwitchClickEvent);
+
+            if (tempSwitchState) {
+
+                Log.d(TAG, "The heater was turned on " + heatOnDateTime);
+                Toast.makeText(this, "The heater was switched ON on " + heatOnDateTime, Toast.LENGTH_LONG).show();
+
+            } else {
+                Log.d(TAG, "The heater was turned off on " + heatOnDateTime);
+                Toast.makeText(this, "The heater was switched OFF on " + heatOnDateTime, Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(this, "HeaterEventId cannot be null", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
