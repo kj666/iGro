ded7e1f071cae5a96a38d519f81078a8d84e121f
mkanyar <33469024+mkanyar@users.noreply.github.com>
2019-04-02 01:50:09 -0400

    dialog fragment
    dialog fragment cannot pop up a toast in the dialog. alternative maybe a button in xml

--- frontend/app/src/main/java/com/example/igro/ChangePasswordDialogFragment.java
+++ frontend/app/src/main/java/com/example/igro/ChangePasswordDialogFragment.java
@@ -9,12 +9,15 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.igro.Controller.Helper;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -29,79 +32,105 @@ import static com.android.volley.VolleyLog.TAG;
 
 public class ChangePasswordDialogFragment extends AppCompatDialogFragment {
 
-        EditText oldPasswordEditText;
-        EditText newPasswordEditText;
-        EditText confirmNewPasswordEditText;
-        String oldPassword;
-        String newPassword;
-        String confirmNewPassword;
-        private FirebaseAuth mAuth; //authentication instance
-    FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        public Dialog onCreateDialog(Bundle savedInstanceState){
+    EditText oldPasswordEditText;
+    EditText newPasswordEditText;
+    EditText confirmNewPasswordEditText;
+    String oldPassword;
+    String newPassword;
+    String confirmNewPassword;
+    Button changePasswordButton;
+    private Helper helper;
+
+    private FirebaseAuth mAuth; //authentication instance
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
 
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_change_password, null);
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-            LayoutInflater inflater = getActivity().getLayoutInflater();
-            View view = inflater.inflate(R.layout.activity_change_password,null);
-
-            builder.setView(view);
-            //initialization
-            oldPasswordEditText = view.findViewById(R.id.oldPasswordEditText);
-            newPasswordEditText = view.findViewById(R.id.newPasswordEditText);
-            confirmNewPasswordEditText = view.findViewById(R.id.confirmNewPasswordEditText);
-            oldPassword=oldPasswordEditText.getText().toString();
-            newPassword=newPasswordEditText.getText().toString();
-            confirmNewPassword=newPasswordEditText.getText().toString();
-            builder.setTitle("Change Password");
-            builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    oldPassword = oldPasswordEditText.getText().toString();
-                    newPassword = newPasswordEditText.getText().toString();
-                    confirmNewPassword = confirmNewPasswordEditText.getText().toString();
-                    validateCurrentPassword(oldPassword,newPassword,confirmNewPassword);
-                }
-            });
-
-            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    // User cancelled the dialog
-                }
-            });
-
-
-            // Create the AlertDialog object and return it
-            return builder.create();
-
-        }
-
-    //check if current password is valid
-    private void validateCurrentPassword(String oldPassword, String newPassword, final String confirmNewPassword) {
-
-        AuthCredential credential= EmailAuthProvider.getCredential(user.getEmail(),oldPassword);
-        user.reauthenticate(credential).addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+        builder.setView(view);
+        //initialization
+        oldPasswordEditText = view.findViewById(R.id.oldPasswordEditText);
+        newPasswordEditText = view.findViewById(R.id.newPasswordEditText);
+        confirmNewPasswordEditText = view.findViewById(R.id.confirmNewPasswordEditText);
+        changePasswordButton=view.findViewById(R.id.changePasswordButton);
+        oldPassword = oldPasswordEditText.getText().toString();
+        newPassword = newPasswordEditText.getText().toString();
+        confirmNewPassword = newPasswordEditText.getText().toString();
+        builder.setTitle("Change Password");
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(oldPassword) && !TextUtils.isEmpty(newPassword)
+                        && !TextUtils.isEmpty(confirmNewPassword)) {
 
-                if (task.isSuccessful()) {
-                    //authentication successful
-                    Log.d(TAG, "Authentication validated to change password");
-                user.updatePassword(confirmNewPassword);
-                } else {
-                    //authentication failed
-                    Log.w(TAG, "Authentication failed in order to change password", task.getException());
-                    Toast.makeText(getActivity(), "The OLD PASSWORD IS WRONG!!!",
-                            Toast.LENGTH_SHORT).show();
+                    validateCurrentPassword(oldPassword, newPassword, confirmNewPassword);
+                     Toast.makeText(getActivity().getApplicationContext(), "Password Changed Successfully"
+                            , Toast.LENGTH_LONG).show();
+                    helper.signout();
+                    helper.goToActivity(LoginActivity.class);
+
                 }
+                else{
+                    Toast.makeText(getActivity().getApplicationContext(), "Please fill in all the fields!!!"
+                            , Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        /*builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                oldPassword = oldPasswordEditText.getText().toString();
+                newPassword = newPasswordEditText.getText().toString();
+                confirmNewPassword = confirmNewPasswordEditText.getText().toString();
+                validateCurrentPassword(oldPassword, newPassword, confirmNewPassword);
+            }
+        });*/
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
             }
         });
 
 
+        // Create the AlertDialog object and return it
+        return builder.create();
+
+    }
+
+    //check if current password is valid
+    private void validateCurrentPassword(String oldPassword,  String newPassword, final String confirmNewPassword) {
+
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), oldPassword);
+            user.reauthenticate(credential).addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    if (task.isSuccessful()) {
+                        //authentication successful
+                        Log.d(TAG, "Authentication validated to change password");
+
+                            user.updatePassword(confirmNewPassword);
+
+                        }
+                     else {
+                        //authentication failed
+                        Log.w(TAG, "Authentication failed in order to change password", task.getException());
+                        Toast.makeText(getActivity(), "The OLD PASSWORD IS WRONG!!!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+
     }
 
 
-    }
+}
+
 
 
