678ddceab989c970e24f5fc8040ec72d2a749b47
kj666 <karthi-kan@hotmail.com>
2019-04-02 01:17:04 -0400

    setup push notification

--- frontend/app/src/main/java/com/example/igro/LoginActivity.java
+++ frontend/app/src/main/java/com/example/igro/LoginActivity.java
@@ -17,6 +17,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -59,6 +61,24 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else{
                     Validate(userName, password);
+
+                    //get token
+                    FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                            if(!task.isSuccessful()){
+                                Log.w(TAG, "getInstanceId failed", task.getException());
+                                return;
+                            }
+                            //Get new instance ID token
+                            String token = task.getResult().getToken();
+
+                            //toast
+                            String msg = getString(R.string.msg_token_fmt, token);
+                            Log.d(TAG, msg);
+                            Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
